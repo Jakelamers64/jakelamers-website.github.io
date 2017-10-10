6a31d92b48f14b04a106506c487a05980c104ff6
Jake Lamers <jakelamers@Jakes-MacBook-Pro.local>
2017-10-10 18:02:14 -0500

    first commit

---
+++ mainScript.js
@@ -0,0 +1,13 @@
+function check(form) {
+	
+	if(form.userid.value == "Jakelamers931" && form.pswrd.value == "Bigred77"){
+		
+		window.open('http://www.mcpasd.k12.wi.us/')
+	
+	}else{
+	
+	alert("username and password not found")
+	
+	}
+}
+
