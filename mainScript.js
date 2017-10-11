9b563c05f0e391c565482097d0f0bcb365de769b
Jake Lamers <jakelamers@Jakes-MacBook-Pro.local>
2017-10-10 20:33:54 -0500

    second commit

--- mainScript.js
+++ mainScript.js
@@ -1,13 +1,10 @@
+/* Password area code*/
 function check(form) {
-	
-	if(form.userid.value == "Jakelamers931" && form.pswrd.value == "Bigred77"){
-		
-		window.open('http://www.mcpasd.k12.wi.us/')
-	
-	}else{
-	
-	alert("username and password not found")
-	
+    
+	if (form.userid.value === "Jakelamers931" && form.pswrd.value === "Bigred77") {
+      window.open('http://www.mcpasd.k12.wi.us/');
+	} else {
+	  alert("username and password not found");
 	}
 }
 
