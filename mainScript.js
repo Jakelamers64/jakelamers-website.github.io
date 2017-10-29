bb82f2253dbec648f2341040e61b6bd0976f21c5
Jake Lamers <jakelamers@MacBook-Air-49.localdomain>
2017-10-29 16:59:26 -0500

    forth commit

--- mainScript.js
+++ mainScript.js
@@ -1,10 +1,30 @@
 /* Password area code*/
 function check(form) {
     
-	if (form.userid.value === "Jakelamers931" && form.pswrd.value === "Bigred77") {
+    if (form.userid.value === "Jakelamers931" && form.pswrd.value === "Bigred77") {
       window.open('http://www.mcpasd.k12.wi.us/');
 	} else {
-	  alert("username and password not found");
+      alert("username and password not found");
 	}
 }
 
+
+//change background
+
+var body = document.getElementById('backgroundArea');
+
+var myTimer = setInterval(changeBackground, 10000);
+
+function changeBackground() {
+    var classArray = ["image", "image2", "image3", "image4"];
+    
+    var imageNumber = classArray[Math.floor(Math.random() * classArray.length)];
+    
+    document.body.setAttribute("class", "" + imageNumber ); 
+} 
+
+
+
+
+
+ 
