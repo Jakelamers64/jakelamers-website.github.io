eb15826aafc177933fba4f832f97d910dd83ee7e
Jakelamers64 <32250381+Jakelamers64@users.noreply.github.com>
2017-10-29 19:37:13 -0500

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -2,7 +2,7 @@
 function check(form) {
     
     if (form.userid.value === "Jakelamers931" && form.pswrd.value === "Bigred77") {
-      window.open('http://www.mcpasd.k12.wi.us/');
+      window.open('https://jakelamers64.github.io/HomePageJL/.');
 	} else {
       alert("username and password not found");
 	}
